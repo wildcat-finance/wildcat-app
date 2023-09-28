60f8999f1205de54c72698670187d113e597476a
Eugene Allenov <e.allenov@gmail.com>
2023-09-28 18:51:01 +0300

    fix btn

--- src/components/ui-components/Button/index.tsx
+++ src/components/ui-components/Button/index.tsx
@@ -6,7 +6,7 @@ import { ButtonProps } from "./interface";
 
 export const Button = ({ variant, disabled, children, onClick, className, icon }: ButtonProps) => {
     const cssClass = cn(
-        'text-white text-xxs py-2 px-5 rounded-full',
+        'text-white text-xxs min-w-18 h-8 px-5 rounded-full',
         'flex items-center justify-center',
         `wc-btn-${variant}`,
         { 'bg-gray cursor-not-allowed': disabled},
