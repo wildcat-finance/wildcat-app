96c58670621f9b6fedfd8618663614f4c73bbbfb
Eugene Allenov <e.allenov@gmail.com>
2023-11-16 12:48:08 +0300

    routing without wallet

--- src/hooks/useWalletConnect.ts
+++ src/hooks/useWalletConnect.ts
@@ -1,21 +1,16 @@
 import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
 import { useAccount } from "wagmi"
 
 import { useWalletConnectModalStore } from "../store/useWalletConnectModalStore"
-import { BASE_PATHS } from "../routes/constants"
 import { useCurrentNetwork } from "./useCurrentNetwork"
 
 export const useWalletConnect = () => {
   const { isConnected } = useAccount()
   const { isWrongNetwork } = useCurrentNetwork()
   const { isOpen, setIsWalletModalOpen } = useWalletConnectModalStore()
-  const navigate = useNavigate()
 
   useEffect(() => {
     if (!isConnected || isWrongNetwork) {
-      navigate(BASE_PATHS.Borrower)
-
       if (!isOpen) {
         setIsWalletModalOpen(true)
       }
