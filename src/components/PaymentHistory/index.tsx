9025a2a87b92b78e9e8ae8a3fb51f10434ca8de8
Eugene Allenov <e.allenov@gmail.com>
2023-11-19 02:26:17 +0300

    fix Borrow history

--- src/components/PaymentHistory/index.tsx
+++ src/components/PaymentHistory/index.tsx
@@ -11,9 +11,8 @@ import {
   TableCell,
   TableRow,
 } from "../ui-components"
-import { CancelRoundBlack, ExpandMore } from "../ui-components/icons"
-import { PaymentHistoryDetailsProps } from "./type"
-import { useGetRepayments } from "./useGetRepayments"
+import { ExpandMore } from "../ui-components/icons"
+import { PaymentHistoryDetailsProps } from "./interface"
 import {
   formatTokenAmount,
   TOKEN_FORMAT_DECIMALS,
@@ -37,14 +36,12 @@ const PaymentHistory = ({ market }: PaymentHistoryDetailsProps) => {
   const [showHistory, setShowHistory] = useState(true)
   const [dateArray, setDateArray] = useState<DateValue[]>([])
 
-  const { fromTimestamp, toTimestamp } = useMemo(
-    () => getFromToTimestamps(dateArray),
-    [dateArray],
-  )
-
-  const { data } = useGetRepayments(market.address, fromTimestamp, toTimestamp)
-
-  const isDatePicked = dateArray.length >= 1
+  // const { fromTimestamp, toTimestamp } = useMemo(
+  //   () => getFromToTimestamps(dateArray),
+  //   [dateArray],
+  // )
+  //
+  // const isDatePicked = dateArray.length >= 1
 
   // const [isActivePage, setIsActivePage] = useState(1)
   //
@@ -56,15 +53,15 @@ const PaymentHistory = ({ market }: PaymentHistoryDetailsProps) => {
   //   return array
   // }
 
-  const handleDateReset = () => {
-    setDateArray([])
-  }
-  const handleSecondDateChange = (date: DateValue) => {
-    setDateArray([dateArray[0], date])
-  }
-  const handleFirstDateChange = (date: DateValue) => {
-    setDateArray([date, dateArray[1]])
-  }
+  // const handleDateReset = () => {
+  //   setDateArray([])
+  // }
+  // const handleSecondDateChange = (date: DateValue) => {
+  //   setDateArray([dateArray[0], date])
+  // }
+  // const handleFirstDateChange = (date: DateValue) => {
+  //   setDateArray([date, dateArray[1]])
+  // }
   const toggleAccordion = () => {
     setShowHistory(!showHistory)
   }
@@ -94,35 +91,35 @@ const PaymentHistory = ({ market }: PaymentHistoryDetailsProps) => {
       </div>
       {showHistory && (
         <div className="mt-8">
-          <div className="flex justify-end items-center mb-5">
-            <div className="flex items-center gap-x-3">
-              <DatePickerInput
-                placeholder="Date From"
-                onChange={handleFirstDateChange}
-                value={dateArray[0]}
-              />
-              <DatePickerInput
-                placeholder="Date To"
-                onChange={handleSecondDateChange}
-                value={dateArray[1]}
-                minValue={dateArray[0]}
-              />
-            </div>
-          </div>
+          {/* <div className="flex justify-end items-center mb-5"> */}
+          {/*  <div className="flex items-center gap-x-3"> */}
+          {/*    <DatePickerInput */}
+          {/*      placeholder="Date From" */}
+          {/*      onChange={handleFirstDateChange} */}
+          {/*      value={dateArray[0]} */}
+          {/*    /> */}
+          {/*    <DatePickerInput */}
+          {/*      placeholder="Date To" */}
+          {/*      onChange={handleSecondDateChange} */}
+          {/*      value={dateArray[1]} */}
+          {/*      minValue={dateArray[0]} */}
+          {/*    /> */}
+          {/*  </div> */}
+          {/* </div> */}
 
-          {isDatePicked && (
-            <div className="flex justify-end items-center mb-5">
-              <Chip className="bg-white w-fit mb-3">
-                {dateArray[0]?.toString()} – {dateArray[1]?.toString()}
-                <CancelRoundBlack
-                  className="ml-2 cursor-pointer"
-                  onClick={handleDateReset}
-                />
-              </Chip>
-            </div>
-          )}
+          {/* {isDatePicked && ( */}
+          {/*  <div className="flex justify-end items-center mb-5"> */}
+          {/*    <Chip className="bg-white w-fit mb-3"> */}
+          {/*      {dateArray[0]?.toString()} – {dateArray[1]?.toString()} */}
+          {/*      <CancelRoundBlack */}
+          {/*        className="ml-2 cursor-pointer" */}
+          {/*        onClick={handleDateReset} */}
+          {/*      /> */}
+          {/*    </Chip> */}
+          {/*  </div> */}
+          {/* )} */}
 
-          {!isDatePicked && <div className="h-8 w-8 mb-3" />}
+          {/* {!isDatePicked && <div className="h-8 w-8 mb-3" />} */}
 
           <Table
             headers={[
@@ -152,47 +149,41 @@ const PaymentHistory = ({ market }: PaymentHistoryDetailsProps) => {
               },
             ]}
           >
-            {data &&
-              data.map((repayment) => (
-                <TableRow key={repayment.blockTimestamp}>
-                  <TableCell justify="start">
-                    <a
-                      className="hover:underline"
-                      href={`https://sepolia.etherscan.io/address/${repayment.from}`}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {trimAddress(repayment.from)}
-                    </a>
-                  </TableCell>
-                  <TableCell justify="start">
-                    <a
-                      className="hover:underline"
-                      href={`https://sepolia.etherscan.io/tx/${repayment.transactionHash}`}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {trimAddress(repayment.transactionHash, 24)}
-                    </a>
-                  </TableCell>
-                  <TableCell justify="start">
-                    {dayjs(repayment.blockTimestamp * 1000).format(DATE_FORMAT)}
-                  </TableCell>
-                  <TableCell justify="start">
-                    {dayjs(repayment.blockTimestamp * 1000).format(DATE_FORMAT)}
-                  </TableCell>
-                  <TableCell justify="end">
-                    {formatTokenAmount(
-                      BigNumber.from(repayment.assetAmount),
-                      underlyingToken.decimals,
-                      TOKEN_FORMAT_DECIMALS,
-                    )}{" "}
-                    {underlyingToken.symbol}
-                  </TableCell>
-                </TableRow>
-              ))}
+            {market.repaymentRecords.map((repayment) => (
+              <TableRow key={repayment.blockTimestamp}>
+                <TableCell justify="start">
+                  <a
+                    className="hover:underline"
+                    href={`https://sepolia.etherscan.io/address/${repayment.from}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {trimAddress(repayment.from)}
+                  </a>
+                </TableCell>
+                <TableCell justify="start">
+                  <a
+                    className="hover:underline"
+                    href={`https://sepolia.etherscan.io/tx/${repayment.transactionHash}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {trimAddress(repayment.transactionHash, 24)}
+                  </a>
+                </TableCell>
+                <TableCell justify="start">
+                  {dayjs(repayment.blockTimestamp * 1000).format(DATE_FORMAT)}
+                </TableCell>
+                <TableCell justify="start">
+                  {dayjs(repayment.blockTimestamp * 1000).format(DATE_FORMAT)}
+                </TableCell>
+                <TableCell justify="end">
+                  {repayment.amount.format(TOKEN_FORMAT_DECIMALS, true)}
+                </TableCell>
+              </TableRow>
+            ))}
           </Table>
-          <Spinner isLoading={!data} fixedDisable className="w-5 h-5 mt-2" />
+          {/* <Spinner isLoading={!data} fixedDisable className="w-5 h-5 mt-2" /> */}
           {/* <div className="flex justify-center gap-x-1 text-xxs mt-6"> */}
           {/*  {numberToArray(4).map((item) => ( */}
           {/*    <button */}
