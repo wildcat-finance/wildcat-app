f1f58e40851c486bae21ba40a3d6558f047af150
Dr Laurence E. Day <laurence.e.day@outlook.com>
2023-11-19 20:12:05 +0000

    Adjusted words of capacity modal for guidance

--- src/pages/borrower/VaultDetails/Modals/CapacityModal/index.tsx
+++ src/pages/borrower/VaultDetails/Modals/CapacityModal/index.tsx
@@ -27,18 +27,13 @@ export const CapacityModal = () => {
 
           <div className="flex flex-col items-center gap-y-5 px-8">
             <div className="w-72 font-light text-xxs text-center">
-              By confirming this transaction, you are changing the base interest
-              rate paid to your lenders from:
+              By confirming this transaction, you are changing the maximum
+              amount of deposits you are comfortable paying interest on from:
             </div>
-            <div className="w-72 font-bold text-xxs text-center">X% to Y%</div>
+            <div className="w-72 font-bold text-xxs text-center">X to Y</div>
             <div className="w-72 font-light text-xxs text-center">
-              [ONLY SHOW IF RATE IS LOWER] The minimum reserve ratio of the
-              market will be temporarily changed to:
-            </div>
-            <div className="w-72 font-bold text-xxs text-center">M%</div>
-            <div className="w-72 font-light text-xxs text-center">
-              To avoid surprises, please make sure you have aligned this change
-              with your active lenders before moving ahead.
+              [NOTE FOR EUGENE] New value cannot be lower than the current
+              totalSupply of the market token - if it is, tx will revert.
             </div>
           </div>
         </div>
