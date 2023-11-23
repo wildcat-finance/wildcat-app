e8e8645dc19257a5fe884dd3f468ff8fbb0f6d41
d1ll0n <dillon@indexed.finance>
2023-11-23 02:34:42 -0600

    Add withdrawal errors display

--- src/pages/lenders/LenderMarketDetails/WithdrawalForm/index.tsx
+++ src/pages/lenders/LenderMarketDetails/WithdrawalForm/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 import { TokenAmount } from "@wildcatfi/wildcat-sdk"
 import { parseUnits } from "ethers/lib/utils"
 import { Button } from "../../../../components/ui-components"
@@ -9,10 +9,12 @@ import {
 import { WithdrawalFormProps } from "./interface"
 import { useWithdraw } from "../../../borrower/BorrowerMarketDetails/hooks/useVaultDetailActions"
 import { DetailsInput } from "../../../../components/ui-components/DetailsInput"
+import { SDK_ERRORS_MAPPING } from "../../../../utils/forms/errors"
 
 const WithdrawalForm = ({ marketAccount }: WithdrawalFormProps) => {
   const { mutateAsync, isLoading } = useWithdraw(marketAccount)
   const [withdrawalValue, setWithdrawalValue] = useState("0")
+  const [error, setError] = useState<string | undefined>()
 
   const withdrawalValueBigNum = new TokenAmount(
     parseUnits(
@@ -22,11 +24,34 @@ const WithdrawalForm = ({ marketAccount }: WithdrawalFormProps) => {
     marketAccount.market.underlyingToken,
   )
 
-  const disabled =
-    withdrawalValueBigNum.raw.isZero() ||
-    withdrawalValueBigNum.gt(marketAccount.market.totalSupply.raw) ||
-    isLoading ||
-    !marketAccount?.canWithdraw
+  const disabled = withdrawalValueBigNum.raw.isZero() || !!error || isLoading
+
+  const clearErrors = () => setError(undefined)
+
+  const validate = (value: string) => {
+    const withdrawalAmount =
+      marketAccount.market.underlyingToken.parseAmount(value)
+
+    const { status } = marketAccount.checkQueueWithdrawalStep(withdrawalAmount)
+
+    if (status !== "Ready") {
+      setError(SDK_ERRORS_MAPPING.queueWithdrawal[status])
+    } else {
+      clearErrors()
+    }
+  }
+
+  const handleChangeWithdrawalAmount = (evt: ChangeEvent<HTMLInputElement>) => {
+    const { value } = evt.target
+    setWithdrawalValue(value)
+
+    if (value === "" || value === "0") {
+      clearErrors()
+      return
+    }
+
+    validate(value)
+  }
 
   const handleWithdraw = () => {
     mutateAsync(withdrawalValue)
@@ -46,10 +71,11 @@ const WithdrawalForm = ({ marketAccount }: WithdrawalFormProps) => {
           value={withdrawalValue}
           className="w-full"
           placeholder="10.00"
-          onChange={(e) => setWithdrawalValue(e.target.value)}
+          onChange={handleChangeWithdrawalAmount}
           min={0}
           max={marketAccount.market.totalSupply.format(TOKEN_FORMAT_DECIMALS)}
           market={marketAccount.market}
+          errorText={error}
           helperText="Balance"
           helperValue={`${marketAccount.marketBalance.format(
             TOKEN_FORMAT_DECIMALS,
