dfce3564ab400ed1dd48f6661fb39f9a8bd9436c
d1ll0n <dillon@indexed.finance>
2023-12-12 16:23:35 -0600

    Wait for subgraph sync on actions

---
+++ src/utils/waitForSubgraphSync.ts
@@ -0,0 +1,71 @@
+import {
+  GetSubgraphStatusDocument,
+  SubgraphGetSubgraphStatusQuery,
+  SubgraphGetSubgraphStatusQueryVariables,
+} from "@wildcatfi/wildcat-sdk/dist/gql/graphql"
+import {
+  ToastId,
+  dismissToast,
+  toastifyInfo,
+  toastifySuccess,
+} from "../components/toasts"
+import { SubgraphClient } from "../config/subgraph"
+
+export async function getSubgraphSyncedBlock(): Promise<{
+  hash: string
+  number: number
+  timestamp: number
+}> {
+  const result = await SubgraphClient.query<
+    SubgraphGetSubgraphStatusQuery,
+    SubgraphGetSubgraphStatusQueryVariables
+  >({
+    query: GetSubgraphStatusDocument,
+  })
+  // eslint-disable-next-line no-underscore-dangle
+  const block = result.data._meta?.block
+  if (!block) {
+    throw Error("Could not get synced block from subgraph")
+  }
+  const { hash, number, timestamp } = block
+  return { hash: hash as string, number, timestamp: timestamp as number }
+}
+
+export async function waitForSubgraphSync(
+  targetBlock: number,
+): Promise<number> {
+  let toastId: ToastId | undefined
+  let initialBlock: number | undefined
+  const check = async (): Promise<number> => {
+    const latestBlock = (await getSubgraphSyncedBlock()).number
+    if (initialBlock === undefined) {
+      // If subgraph is synced on first check, don't show toast
+      if (latestBlock >= targetBlock) {
+        return latestBlock
+      }
+
+      initialBlock = latestBlock
+    }
+    if (latestBlock < targetBlock) {
+      const blocksRemaining = targetBlock - latestBlock
+      console.log(
+        `Waiting for subgraph sync... ${blocksRemaining} blocks remaining`,
+      )
+      const blocksProcessed = latestBlock - initialBlock
+      const totalBlocks = targetBlock - initialBlock
+      const progress = blocksProcessed / totalBlocks
+      if (toastId !== undefined) dismissToast(toastId)
+      toastId = toastifyInfo(
+        `Waiting for subgraph sync. Latest block: ${latestBlock}. Target block: ${targetBlock}.`,
+        { progress },
+      )
+      // eslint-disable-next-line no-promise-executor-return
+      await new Promise((resolve) => setTimeout(resolve, 5000))
+      return check()
+    }
+    if (toastId !== undefined) dismissToast(toastId)
+    toastifySuccess(`Subgraph synced to block ${targetBlock}`)
+    return latestBlock
+  }
+  return check()
+}
