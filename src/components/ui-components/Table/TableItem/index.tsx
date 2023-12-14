f2f435374518de0239472cdd9361389cb999719a
Eugene Allenov <e.allenov@gmail.com>
2023-12-14 19:05:26 +0300

    WIL-161: fix claim table design

--- src/components/ui-components/Table/TableItem/index.tsx
+++ src/components/ui-components/Table/TableItem/index.tsx
@@ -5,13 +5,16 @@ export function TableCell({
   children,
   justify,
   rowSpan,
+  className,
 }: {
   children: ReactNode
   justify: string
   rowSpan?: number
+  className?: string
 }) {
+  const cellClassName = cn(`${className}`, "first:pl-6 last:pr-6")
   return (
-    <td className="first:pl-6 last:pr-6" rowSpan={rowSpan || 1}>
+    <td className={cellClassName} rowSpan={rowSpan || 1}>
       <div
         className={cn(
           `justify-${justify}`,
