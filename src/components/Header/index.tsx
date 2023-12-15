7d33e3648c0a498a5cf8d2a3eccc787936d1e5c6
Eugene Allenov <e.allenov@gmail.com>
2023-12-15 19:13:30 +0300

    WIL-166

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,35 @@
 import * as React from "react"
 
-import { useMedia } from "react-use"
 import { ReactComponent as WildcartLogo } from "../../images/wildcat-logo-white.svg"
 import ConnectButton from "./ConnectButton"
 import NavItem from "./NavItem"
 
 export function Header() {
-  const isDesktop = useMedia("(min-width: 768px)")
+  const isMobile = () => {
+    const toMatch = [
+      /Android/i,
+      /webOS/i,
+      /iPhone/i,
+      /iPad/i,
+      /iPod/i,
+      /BlackBerry/i,
+      /Windows Phone/i,
+    ]
+
+    return toMatch.some((toMatchItem) => navigator.userAgent.match(toMatchItem))
+  }
 
   return (
     <div className="h-20 bg-black px-8 py-4 flex items-center justify-between bg-">
       <WildcartLogo className="h-full" />
-      {isDesktop && (
+      {!isMobile() && (
         <div className="flex flex-1 items-center justify-center gap-10">
           <NavItem name="Borrowers" link="/borrower" linkName="borrower" />
           <NavItem name="Lenders" link="lender" linkName="lender" />
         </div>
       )}
 
-      {isDesktop && <ConnectButton />}
+      {!isMobile() && <ConnectButton />}
     </div>
   )
 }
