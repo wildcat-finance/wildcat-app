e415f7b60a795341dc633a7e4934a7dfdb94d7b2
d1ll0n <dillon@indexed.finance>
2023-12-19 15:41:52 -0600

    resolve borrower routing conflicts

--- src/pages/borrower/routes/constants.ts
+++ src/pages/borrower/routes/constants.ts
@@ -4,4 +4,5 @@ export const BORROWER_PATHS = {
   ServiceAgreement: "agreement",
   AddNewMarket: "add-new-market",
   MarketDetails: "market-details/:marketAddress",
+  PendingRegistration: "pending-registration",
 }
