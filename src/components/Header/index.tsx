03f1758cb08d609cdde9f333a70da808a464d99a
d1ll0n <dillon@wildcat.finance>
2024-03-05 13:44:33 -0800

    make logo redirect to /

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 
+import { Link } from "react-router-dom"
 import { ReactComponent as WildcartLogo } from "../../images/wildcat-logo-white.svg"
 import ConnectButton from "./ConnectButton"
 import NavItem from "./NavItem"
@@ -21,7 +22,9 @@ export function Header() {
 
   return (
     <div className="h-20 bg-black px-8 py-4 flex items-center justify-between bg-">
-      <WildcartLogo className="h-full" />
+      <Link to="/" className="h-full">
+        <WildcartLogo className="h-full" />
+      </Link>
       {!isMobile() && (
         <div className="flex flex-1 items-center justify-center gap-10">
           <NavItem name="Borrowers" link="/borrower" linkName="borrower" />
